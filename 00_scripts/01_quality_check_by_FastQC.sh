6d63be6531cbc8681b607d988e37b26276b42b1e
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2022-06-10 16:09:16 +0400

    Update 01_quality_check_by_FastQC.sh

--- 00_scripts/01_quality_check_by_FastQC.sh
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -5,8 +5,11 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
-WORKING_DIRECTORY=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/01_raw_data
-OUTPUT=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/02_quality_check
+WORKING_DIRECTORY=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/01_raw_data/Original_reads_16S
+OUTPUT=/scratch_vol1/fungi/BioIndic_La_Reunion_Island_Lagoon/02_quality_check/Original_reads_16S
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
 
 eval "$(conda shell.bash hook)"
 conda activate fastqc
